07edbe871af059af4af419634fbde0a1816e7d39
benwata777 <benwata777@gmail.com>
2024-07-03 23:17:07 +0700

    nested loop

--- ex-1.js
+++ ex-1.js
@@ -1,4 +1,16 @@
-// เริ่มเขียนโค้ดตรงนี้ 
-function printPattern() {
-   
+// เริ่มเขียนโค้ดตรงนี้
+function printPattern(n) {
+  let finalResult = "";
+  for (let i = 1; i <= n; i++) {
+    // console.log(i);
+    let rowResult = "";
+    for (let j = 1; j <= i; j++) {
+      rowResult += j + "\t";
+      //   console.log(rowResult);
+    }
+    finalResult += rowResult + "\n";
+  }
+  return finalResult;
 }
+
+console.log(printPattern(5));
--- ex-3.js
+++ ex-3.js
@@ -1,4 +1,20 @@
-// เริ่มเขียนโค้ดตรงนี้ 
-function multiplicationTable() {
-   
+// เริ่มเขียนโค้ดตรงนี้
+function multiplicationTable(n) {
+  let finalResult = "";
+
+  for (let i = 1; i <= n; i++) {
+    let rowResult = "";
+    // console.log(i);//1,2,3,4,5
+    for (let j = 1; j <= n; j++) {
+      rowResult += i * j + "\t";
+    }
+    finalResult += rowResult + "\n";
+  }
+  return finalResult;
 }
+console.log(multiplicationTable(5));
+// 1   2   3   4   5
+// 2   4   6   8  10
+// 3   6   9  12  15
+// 4   8  12  16  20
+// 5  10  15  20  25
